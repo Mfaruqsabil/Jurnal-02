a03eeff8d657ce0945d0bf04980c99a566804251
Mfaruqsabil <mfaruqsabil@student.telkomuniversity.ac.id>
2020-09-16 19:51:05 +0700

    HelloWorld.java

---
+++ pratikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+ public static void main (String[] args) {
+   System.out.println ("Nama: M. Faruq Sabil"); 
+   System.out.println ("NIM: 6706202086");
+   System.out.println ("Kelas: D3IF-44-02");
+ }
+} 
